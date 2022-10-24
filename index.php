693180cb2bfbd842ccca73e32ae1f0615fb5c3e0
nikmatunkhasanah29 <nikmakhasa@gmail.com>
2022-10-24 11:01:33 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Hello guys<br>";
+
+$nama="Nikma";
+$umur=21;
+//catatan : echo digunakan untuk menampilkan variabel
+//keterangan text disebut string, angka = nominal, true/false = bolean
+//cara memisahkan baris menggunakan <br>
+//<strong> digunakan untuk menebalkan text
+echo "Nama saya <strong>$nama</strong>, Saya berusia $umur tahun.<br>";
+
+
+//Aritmatika sederhana
+
+$nama_adik="Diva";
+$umur_adik=8;
+$selisih_umur=$umur-$umur_adik;
+echo "Nama adik saya <strong>$nama_adik</strong>, Saya berusia $umur_adik tahun.<br>";
+
+echo "Selisih usia saya dan adik yaitu $selisih_umur tahun";
+
+?>
